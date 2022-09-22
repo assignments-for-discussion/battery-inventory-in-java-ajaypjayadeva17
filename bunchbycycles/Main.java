10feaaa9f7f9927f87cc63cb49ba2d515e5707a0
ajaypjayadeva17 <103561123+ajaypjayadeva17@users.noreply.github.com>
2022-09-22 18:13:47 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -15,6 +15,18 @@ public class Main {
   static void testBucketingByNumberOfCycles() {
     System.out.println("Counting batteries by usage cycles...\n");
     CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
+    for(int i=0;i<5;i++)
+    {
+    if(chargeCycleCounts[i]<410)     //checking for low count
+    counts.lowCount++;               //incrementing the low count variable everytime by one if the value is less than 410
+    else if(chargeCycleCounts[i]>=410 && chargeCycleCounts[i]<910) //checking  for medium count 
+    counts.mediumCount++;            //incrementing the medium count variable everytime by one if the value is greater than 410 and less than 910
+    else                              //checking for high count
+    counts.highCount++;              // incrementing the high count variable
+    }
+    System.out.println(counts.lowCount);     //displaying the low count
+    System.out.println(counts.mediumCount);  //displaying the medium count
+    System.out.println(counts.highCount);   //displaying the high count
     assert(counts.lowCount == 2);
     assert(counts.mediumCount == 3);
     assert(counts.highCount == 1);
